5487be45f55592522eefce38987ab987baf32879
Keikeu <karolina.m.placek@gmail.com>
2019-03-25 20:05:27 +0100

    follow JavaScript good practices

--- js/script.js
+++ js/script.js
@@ -1,24 +1,27 @@
-let navOpened = false;
-const nav = document.getElementsByClassName("nav--mobile")[0];
-const navItems = document.getElementsByClassName("nav__item");
-const btnOpen = document.getElementsByClassName("nav-btn--open")[0];
-const btnClose = document.getElementsByClassName("nav-btn--close")[0];
+var navOpened = false;
 
 function toggleNav() {
+  let i;
+  const nav = document.getElementsByClassName("nav--mobile")[0];
+  const navItems = document.getElementsByClassName("nav__item");
+  const btnOpen = document.getElementsByClassName("nav-btn--open")[0];
+  const btnClose = document.getElementsByClassName("nav-btn--close")[0];
+
   if (navOpened) {
     nav.style.right = "-100%";
     btnOpen.style.display = "block";
     btnClose.style.display = "none";
-    for(let i = 0; i < navItems.length; i++) {
+    for(i = 0; i < navItems.length; i++) {
       navItems[i].style.opacity = 0;
     }
   } else {
     nav.style.right = "0";
     btnOpen.style.display = "none";
     btnClose.style.display = "block";
-    for(let i = 0; i < navItems.length; i++) {
+    for(i = 0; i < navItems.length; i++) {
       navItems[i].style.opacity = 1;
     }
   }
+
   navOpened = !navOpened;
 }
